d62cecdbed9e58a9cab70703dd3999cf2313be69
Morgan hunter <morgan.hunter@cultureamp.com>
2024-12-10 10:17:42 +1100

    feat: dat 7

--- 2024/07/main.go
+++ 2024/07/main.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"regexp"
 	"strconv"
 )
 
 var (
-	permutations = make(map[int][][]string)
-	total        = 0
+	total = 0
+	match = false
 )
 
 func main() {
@@ -20,7 +19,7 @@ func main() {
 		log.Fatal("Usage: program <parts> <mode>")
 	}
 
-	// part := os.Args[1]
+	part := os.Args[1]
 	mode := os.Args[2]
 
 	file, _ := os.Open(fmt.Sprintf("2024/07/%s.txt", mode))
@@ -30,98 +29,51 @@ func main() {
 
 	re := regexp.MustCompile(`[0-9]+`)
 
-	// var options map[int][][]string
-
 	for scanner.Scan() {
 		line := scanner.Text()
-		elements := arrayToInt(re.FindAllString(line, -1))
-		solution := elements[0]
-		numbers := elements[1:]
+		elements := re.FindAllString(line, -1)
+		solution := toInt(elements[0])
+		numbers := arrayToInt(elements[1:])
 
-		if len(numbers) == 1 {
-			if numbers[0] == solution {
-				total++
-			}
-			continue
-		}
+		match = false
+		calculate(numbers, solution, part)
 
-		if permutations[len(numbers)-1] == nil {
-			permutations[len(numbers)-1] = permute([]string{"+", "*"}, len(numbers)-1)
-		}
-
-		for _, p := range permutations[len(numbers)-1] {
-			tmp := numbers[0]
-			for i := 0; i < len(p); i++ {
-				if p[i] == "+" {
-					tmp += numbers[i+1]
-				} else {
-					tmp *= numbers[i+1]
-				}
-			}
-
-			if tmp == solution {
-				total += solution
-				break
-			}
+		if match {
+			total += solution
 		}
 	}
 
 	fmt.Println(total)
 }
 
-func permute(choices []string, spots int) [][]string {
-	res := make([][]string, 0)
-
-	var first, last []string
-	for i := 0; i < spots; i++ {
-		first = append(first, choices[0])
-		last = append(last, choices[1])
+func calculate(input []int, solution int, part string) {
+	if len(input) == 1 {
+		if input[0] == solution {
+			match = true
+		}
+		return
 	}
 
-	res = append(res, first)
-
-	var backTrack func([]string)
-	backTrack = func(variation []string) {
-		if reflect.DeepEqual(variation, last) {
-			return
-		}
-
-		temp := make([]string, 0)
-		temp = append(temp, variation...)
-
-		lastStarIdx := getLastIndex(variation, "*")
-		lastPlusIdx := getLastIndex(variation, "+")
-
-		if lastStarIdx == -1 {
-			// e.g. +++ -> ++*
-			temp[lastPlusIdx] = "*"
-		} else if lastPlusIdx < lastStarIdx {
-			// e.g. +++* -> ++*+
-			for i := lastPlusIdx; i < len(temp); i++ {
-				temp[i] = "+"
-			}
-			temp[lastPlusIdx] = "*"
-		} else if lastPlusIdx > lastStarIdx {
-			// e.g. +*++ -> +*+*
-			temp[lastPlusIdx] = "*"
-		}
-
-		res = append(res, temp)
-		backTrack(temp)
+	var operations []string
+	if part == "1" {
+		operations = []string{"+", "*"}
+	} else {
+		operations = []string{"+", "*", "||"}
 	}
 
-	backTrack(first)
-	return res
-}
-
-func getLastIndex(input []string, value string) int {
-	for i := len(input) - 1; i >= 0; i-- {
-		if input[i] == value {
-			return i
+	for _, o := range operations {
+		switch o {
+		case "+":
+			tmp := input[0] + input[1]
+			calculate(append([]int{tmp}, input[2:]...), solution, part)
+		case "*":
+			tmp := input[0] * input[1]
+			calculate(append([]int{tmp}, input[2:]...), solution, part)
+		case "||":
+			tmp := toInt(fmt.Sprintf("%d%d", input[0], input[1]))
+			calculate(append([]int{tmp}, input[2:]...), solution, part)
 		}
 	}
-
-	return -1
 }
 
 func toInt(s string) int {
