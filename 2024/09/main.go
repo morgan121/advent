2eb112ec8d34d4e3bbcea3547da5634c0223c9f7
Morgan hunter <morgan.hunter@cultureamp.com>
2024-12-11 17:03:17 +1100

    wip: day 9 attempt for part 2

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -10,9 +10,17 @@ import (
 	"strconv"
 )
 
+type Block struct {
+	value    int
+	length   int
+	startIdx int
+}
+
 var (
 	blockLength []int
 	gapLength   []int
+	blocks      []Block
+	gaps        []Block
 )
 
 func main() {
@@ -32,7 +40,7 @@ func main() {
 	case "1":
 		calculatePart1()
 	case "2":
-		calculatePart2()
+		calculatePart2() // 8317688930597 is too high
 	}
 
 }
@@ -51,39 +59,52 @@ func calculatePart1() {
 }
 
 func calculatePart2() {
-}
+	disc := condenseByBlock(translate(blockLength, gapLength))
+	total := 0
 
-func condense(disc []int) []int {
-	processing := true
-	for ok := true; ok; ok = processing {
-		firstGapIdx := slices.Index(disc, -1)
-		lastBlockIdx := lastNonZeroIndex(disc)
-
-		if firstGapIdx > lastBlockIdx {
-			processing = false
-		} else {
-			lastBlockValue := disc[lastBlockIdx]
-			disc[firstGapIdx] = lastBlockValue
-			disc[lastBlockIdx] = -1
+	for i, value := range disc {
+		if value != -1 {
+			total += value * i
 		}
 	}
 
-	return disc
+	fmt.Println(total)
 }
 
 func translate(blockLength []int, gapLength []int) []int {
 	var translated []int
 
+	blocks = make([]Block, 0)
+	gaps = make([]Block, 0)
+
 	initialBlock := makeBlock(0, blockLength[0])
 	translated = append(translated, initialBlock...)
+	blocks = append(blocks, Block{length: len(initialBlock), value: 0, startIdx: 0})
 	for i := 1; i < len(blockLength); i++ {
 		gap := makeBlock(-1, gapLength[i-1])
 		block := makeBlock(i, blockLength[i])
+		gaps = append(gaps, Block{length: gapLength[i-1], value: -1, startIdx: len(translated)})
 		translated = append(translated, gap...)
+		blocks = append(blocks, Block{length: blockLength[i], value: i, startIdx: len(translated)})
 		translated = append(translated, block...)
 	}
 
 	return translated
+}
+
+func condense(disc []int) []int {
+	firstGapIdx := slices.Index(disc, -1)
+	lastBlockIdx := lastNonZeroIndex(disc)
+
+	if firstGapIdx > lastBlockIdx {
+		return disc
+	} else {
+		disc[firstGapIdx] = disc[lastBlockIdx]
+		disc[lastBlockIdx] = -1
+		condense(disc)
+	}
+
+	return disc
 }
 
 func lastNonZeroIndex(input []int) int {
@@ -94,6 +115,36 @@ func lastNonZeroIndex(input []int) int {
 	}
 
 	return -1
+}
+
+func condenseByBlock(disc []int) []int {
+	for i := len(blocks) - 1; i >= 0; i-- {
+
+		block := blocks[i]
+		firstAppropriateGapIdx := slices.IndexFunc(gaps, func(gap Block) bool {
+			return gap.length >= block.length
+		})
+
+		if firstAppropriateGapIdx == -1 {
+			continue
+		} else {
+			gap := gaps[firstAppropriateGapIdx]
+
+			for g := gap.startIdx; g < gap.startIdx+block.length; g++ {
+				disc[g] = block.value
+			}
+
+			for b := block.startIdx; b < block.startIdx+block.length; b++ {
+				disc[b] = -1
+			}
+
+			gap.startIdx += block.length
+			gap.length -= block.length
+			gaps[firstAppropriateGapIdx] = gap
+		}
+	}
+
+	return disc
 }
 
 func makeBlock(value int, length int) []int {
