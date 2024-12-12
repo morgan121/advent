fafb61d0e8b356287235e55699e52af8bc54b955
Morgan hunter <morgan.hunter@cultureamp.com>
2024-12-12 19:46:05 +1100

    feat: day 11 part 2

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -6,12 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
-	"slices"
 	"strconv"
-)
-
-var (
-	stones = make([]int, 0)
 )
 
 func main() {
@@ -25,43 +20,100 @@ func main() {
 	file, _ := os.Open(fmt.Sprintf("2024/11/%s.txt", mode))
 	defer file.Close()
 
-	parse(file)
+	initialStones := parse(file)
 
-	switch part {
-	case "1":
-		for i := 0; i < 25; i++ {
-			blink()
-		}
-		fmt.Println(len(stones))
-	case "2":
-	}
+	blinkCount := map[string]int{
+		"1": 25,
+		"2": 75,
+	}[part]
+
+	total := simulateStones(initialStones, blinkCount)
+	fmt.Println(total)
 }
 
-func blink() {
-	for i := 0; i < len(stones); i++ {
-		convertedStone := convertStone(stones[i])
-		stones = slices.Delete(stones, i, i+1)
-		stones = slices.Insert(stones, i, convertedStone...)
-
-		if len(convertedStone) > 1 {
-			i++
-		}
-	}
-}
-
-func convertStone(stone int) []int {
-	if stone == 0 {
-		return []int{1}
-	} else if len(strconv.Itoa(stone))%2 == 0 {
-		stringStone := strconv.Itoa(stone)
-		return []int{
-			toInt(stringStone[:len(stringStone)/2]),
-			toInt(stringStone[len(stringStone)/2:]),
-		}
+func simulateStones(initialStones []int, blinks int) int {
+	// Map to track the count of each stone
+	stoneCounts := make(map[int]int)
+	for _, stone := range initialStones {
+		stoneCounts[stone]++
 	}
 
-	return []int{stone * 2024}
+	for i := 0; i < blinks; i++ {
+		nextStoneCounts := make(map[int]int)
+		for stone, count := range stoneCounts {
+			if stone == 0 {
+				nextStoneCounts[1] += count
+			} else {
+				strStone := strconv.Itoa(stone)
+				if len(strStone)%2 == 0 {
+					halfLen := len(strStone) / 2
+					left, right := toInt(strStone[:halfLen]), toInt(strStone[halfLen:])
+					nextStoneCounts[left] += count
+					nextStoneCounts[right] += count
+				} else {
+					nextStoneCounts[stone*2024] += count
+				}
+			}
+		}
+		stoneCounts = nextStoneCounts
+	}
+
+	// Calculate the total number of stones
+	totalStones := 0
+	for _, count := range stoneCounts {
+		totalStones += count
+	}
+	return totalStones
 }
+
+// func processStones(blinkCount int) int {
+// 	total := 0
+
+// 	for _, stone := range stones {
+// 		if stoneCount[stone] == 0 {
+// 			transformStone(stone, stone, 0, blinkCount)
+// 		}
+// 		total += stoneCount[stone]
+// 	}
+
+// 	return total
+// }
+
+// func transformStone(stone int, initialStone int, iterations int, maxIterations int) {
+// 	if iterations == maxIterations {
+// 		return
+// 	}
+
+// 	if _, exists := converter[stone]; !exists {
+// 		converter[stone] = convertStone(stone)
+// 	}
+
+// 	newStones := converter[stone]
+// 	if len(newStones) == 2 {
+// 		stoneCount[initialStone]++
+// 	}
+
+// 	for _, stone := range newStones {
+// 		transformStone(stone, initialStone, iterations+1, maxIterations)
+// 	}
+// }
+
+// func convertStone(stone int) []int {
+// 	if stone == 0 {
+// 		return []int{1}
+// 	}
+
+// 	strStone := strconv.Itoa(stone)
+// 	if len(strStone)%2 == 0 {
+// 		halfLen := len(strStone) / 2
+// 		return []int{
+// 			toInt(strStone[:halfLen]),
+// 			toInt(strStone[halfLen:]),
+// 		}
+// 	}
+
+// 	return []int{stone * 2024}
+// }
 
 func arrayToInt(s []string) []int {
 	var intArray []int
@@ -82,13 +134,17 @@ func toInt(s string) int {
 	return n
 }
 
-func parse(file *os.File) {
+func parse(file *os.File) []int {
 	scanner := bufio.NewScanner(file)
 	re := regexp.MustCompile(`\d+`)
+
+	var stones []int
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		numbers := arrayToInt(re.FindAllString(line, -1))
 		stones = append(stones, numbers...)
 	}
+
+	return stones
 }
