6e7184e8ed84d70af4187a8d3c545eebf3ada33c
Morgan hunter <morgan.hunter@cultureamp.com>
2024-12-19 17:20:45 +1100

    feat: day 14 part 2

--- 2024/14/main.go
+++ 2024/14/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
@@ -40,7 +41,75 @@ func main() {
 		}
 		calcQuadrants(robots, xMax, yMax)
 	case "2":
+		counter := 0
+		for !hasTree(robots) {
+			counter++
+			robots = step(robots, xMax, yMax)
+		}
+
+		fmt.Println(counter)
 	}
+}
+
+func hasTree(robots []Robot) bool {
+	// attempt 1: 6 points in a row = a tree exists
+	horizontals := make(map[int][]int)
+	verticals := make(map[int][]int)
+
+	for _, robot := range robots {
+		horizontals[robot.xPos] = append(horizontals[robot.xPos], robot.yPos)
+		verticals[robot.yPos] = append(verticals[robot.yPos], robot.xPos)
+	}
+
+	// horizontal line
+	keys := make([]int, 0, len(horizontals))
+	for key := range horizontals {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return len(horizontals[keys[i]]) > len(horizontals[keys[j]])
+	})
+
+	if len(horizontals[keys[0]]) > 6 {
+		return isSequential(horizontals[keys[0]])
+	}
+
+	// vertical line
+	keys = make([]int, 0, len(verticals))
+	for key := range verticals {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return len(verticals[keys[i]]) > len(verticals[keys[j]])
+	})
+
+	if len(verticals[keys[0]]) > 6 {
+		return isSequential(verticals[keys[0]])
+	}
+
+	return false
+}
+
+func isSequential(arr []int) bool {
+	sort.Ints(arr)
+
+	seqCounter := 0
+
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i]+1 != arr[i+1] {
+			seqCounter = 0
+		} else {
+			seqCounter++
+		}
+
+		if seqCounter >= 6 {
+			return true
+		}
+	}
+
+	return false
 }
 
 func calcQuadrants(robots []Robot, xMax int, yMax int) {
